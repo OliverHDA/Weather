57e2899e11ba010c3d7feb5700c9ae7f27a3a483
OliverHDA <OliverHD@yandex.ru>
2020-10-16 00:47:56 +0300

    HW4

---
+++ app/src/main/java/ru/oliverhd/weather/Constants.java
@@ -0,0 +1,6 @@
+package ru.oliverhd.weather;
+
+public interface Constants {
+    String TEXT = "TEXT";
+    String RESULT_TEXT = "RESULT_TEXT";
+}
---
+++ app/src/main/java/ru/oliverhd/weather/Parcel.java
@@ -0,0 +1,8 @@
+package ru.oliverhd.weather;
+
+import java.io.Serializable;
+
+public class Parcel implements Serializable {
+
+    public String text;
+}
