cd2ea3db9975915f2d1a9d0de448e4694c90b801
OliverHDA <OliverHD@yandex.ru>
2020-11-06 00:44:20 +0300

    HW1

--- app/src/main/java/ru/oliverhd/weather/MainActivity.java
+++ app/src/main/java/ru/oliverhd/weather/MainActivity.java
@@ -5,6 +5,12 @@ import android.content.res.Configuration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +30,26 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.cities, new CitiesFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem search = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) search.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Snackbar.make(searchView, query, BaseTransientBottomBar.LENGTH_LONG).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
+        return true;
     }
 }
