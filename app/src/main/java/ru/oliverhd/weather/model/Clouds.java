c5fefba4e35c233279ecdfcd762be1eba9281530
OliverHDA <OliverHD@yandex.ru>
2020-11-05 23:06:18 +0300

    HW8

---
+++ app/src/main/java/ru/oliverhd/weather/model/Clouds.java
@@ -0,0 +1,14 @@
+package ru.oliverhd.weather.model;
+
+public class Clouds {
+    private int all;
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+}
+
